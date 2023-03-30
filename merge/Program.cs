1638fb5b2f0823000de142e6f8b2e7a6235b693d
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 16:54:56 +0530

    Add project files.

---
+++ merge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[5] { 1, 2, 3, 4, 5 };
+            int[] arr2 = new int[5] { 6, 7, 8, 9, 10 };
+            //int[] merged = new int[10];
+            int[] merged = arr1.Concat(arr2).ToArray();
+            for(int i=0;i< merged.Length;i++)
+            {
+                Console.Write(merged[i] + " ");
+            }
+        }
+    }
+}
